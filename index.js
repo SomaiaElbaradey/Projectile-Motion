b48677c95eb976242e565cb69e51269727d25767
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-06-26 16:26:37 +0200

    chart: motion and design

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 let startBtn = document.getElementById("startBtn");
-let initialVelocity = 50;
-let angle = 30; // in degrees;
-const gravity = 9.8;
-
-let angle = document.getElementById("angle");
+let restartBtn = document.getElementById("restartBtn");
 let velocity = document.getElementById("velocity");
-
+let angle = document.getElementById("angle");
+let velocityNumber = document.getElementById("velocityNumber");
+let angleNumber = document.getElementById("angleNumber");
+let initialVelocity = velocity.value;
+let initialAngle = angle.value;
+let chart;
 let timeInSeconds = 0;
 let intervalId = null;
+const gravity = 9.8;
 
-JSC.Chart('chartDiv', {});
+//initialize the chart
+window.onload = function () {
+  chart = new CanvasJS.Chart("motionChart", {
+    backgroundColor: "#00000",
+    axisX: {
+      title: "X Axis",
+      titleFontColor: "#ffff",
+      lineColor: "#369EAD",
+    },
+    axisY: {
+      title: "Y Axis",
+      titleFontColor: "#ffff",
+      lineColor: "#369EAD",
+    },
+    title: {
+      text: "Trajectory",
+      fontColor: "#ffff",
+    },
+    animationEnabled: true,
+    data: [
+      {
+        type: "line",
+        dataPoints: [],
+      },
+    ],
+  });
+  chart.render();
+};
 
-let initialYVelocity = function (initialV = initialVelocity, theta = angle) {
+let addPoint = function (xPoint, yPoint) {
+  chart.data[0].addTo("dataPoints", { x: xPoint, y: yPoint });
+};
+
+let initialYVelocity = function (
+  initialV = initialVelocity,
+  theta = initialAngle
+) {
   return initialV * Math.sin(toRadians(theta));
 };
 
@@ -19,7 +55,7 @@ let timeOfFlight = function () {
   return (2 * initialYVelocity()) / gravity;
 };
 
-let xVelocity = function (initialV = initialVelocity, theta = angle) {
+let xVelocity = function (initialV = initialVelocity, theta = initialAngle) {
   return initialV * Math.cos(toRadians(theta));
 };
 let yVelocity = function (time = timeInSeconds) {
@@ -31,39 +67,23 @@ let xDisplacement = function (time = timeInSeconds) {
 };
 let yDisplacement = function (time = timeInSeconds) {
   return (
-    initialVelocity * Math.sin(toRadians(angle)) * time -
+    initialVelocity * Math.sin(toRadians(initialAngle)) * time -
     0.5 * gravity * Math.pow(time, 2)
   );
 };
 
-// angle.addEventListener("input", function (event) {
-//     document.getElementById("angleVal").nodeValue = event.value
-//   });
-
-
 function startProjectile() {
   //Reset any previous movement.
   resetTimer();
-  //Start new one and make it run every 1 second.
+  //Start new one and make it run every 0.5 second.
   startMovement();
-  intervalId = setInterval(startMovement, 1000);
+  intervalId = setInterval(startMovement, 500);
 }
 
 function startMovement() {
-//   var c = document.getElementById("myCanvas");
-//   var ctx = c.getContext("2d");
-
-//   // Create gradient
-//   var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
-//   grd.addColorStop(0, "red");
-//   grd.addColorStop(1, "white");
-
-//   // Fill with gradient
-//   ctx.fillStyle = grd;
-//   ctx.fillRect(10, 10, 150, 80);
-
   if (timeInSeconds > timeOfFlight()) {
     let flightTime = timeOfFlight();
+    addPoint(xDisplacement(flightTime), 0);
     console.log(
       `at time (${flightTime})sec: Vx(constant)=${xVelocity()}, Vy=${yVelocity(
         flightTime
@@ -72,15 +92,27 @@ function startMovement() {
     resetTimer();
     return;
   }
+  addPoint(xDisplacement(), yDisplacement());
 
   console.log(
     `at time (${timeInSeconds})sec: Vx(constant)=${xVelocity()}, Vy=${yVelocity()}, x=${xDisplacement()}, y=${yDisplacement()}`
   );
-  timeInSeconds += 1;
+  timeInSeconds += 0.5;
 }
 
-startBtn.addEventListener("click", function (event) {
+startBtn.addEventListener("click", function () {
+  chart.data[0].set("dataPoints", []);
+  initialVelocity = velocity.value;
+  initialAngle = angle.value;
   startProjectile();
+});
+
+restartBtn.addEventListener("click", function () {
+  velocity.value = 50;
+  angle.value = 45;
+  angleNumber.innerText = angle.value;
+  velocityNumber.innerText = velocity.value;
+  chart.data[0].set("dataPoints", []);
 });
 
 function resetTimer() {
@@ -88,6 +120,6 @@ function resetTimer() {
   intervalId != null ? clearInterval(intervalId) : null;
 }
 
-function toRadians(angle) {
-  return angle * (Math.PI / 180);
+function toRadians(initialAngle) {
+  return initialAngle * (Math.PI / 180);
 }
